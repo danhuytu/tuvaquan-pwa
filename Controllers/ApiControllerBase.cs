9e25bedfcac58511aa96595be64cd4b467e97110
danhuytu <lehoanglambinh@gmail.com>
2026-08-15 21:54:04 +0700

    Add files via upload

--- Controllers/ApiControllerBase.cs
+++ Controllers/ApiControllerBase.cs
@@ -7,6 +7,7 @@ namespace guithu.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+[IgnoreAntiforgeryToken]
 public abstract class ApiControllerBase : ControllerBase
 {
     protected int CurrentUserId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
